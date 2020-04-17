9b1f7ab0799886d74a46afe705b67a87296c8db4
Eric Hippler <hippler.eric@gmail.com>
2020-04-17 10:51:25 -0400

    Updated course information and summary

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,30 @@
 $(document).ready(function(){
 
-    $("#js-theme-toggle-button").click(function(){
+    $(".js-theme-toggle-button").click(function(){
+        toggleStylesheet();
+    });
+
+    function toggleStylesheet(){
         let sheet = document.getElementById("site-style");
-        if (sheet.getAttribute("href") == "summer-styles.css"){
+        if (sheet.getAttribute("href") === "summer-styles.css"){
             sheet.setAttribute("href", "winter-styles.css");
         }
         else {
             sheet.setAttribute("href", "summer-styles.css");
         }
+    };
+
+    //TODO Collapse navbar when nav-link is clicked
+/*     
+    $(".nav-link").click(function(){
+        //console.log("clicked!");
+        //navToggleBtn[0].setAttribute("class", "navbar-toggler collapsed");
+        //navToggleBtn[0].setAttribute("aria-expanded", "false");
+        $("#navbar").collapse('toggle');
+
     });
+ */ 
+
+    
 
 });
